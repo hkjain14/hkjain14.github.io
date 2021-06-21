085b4b1e9dc89a445908780fc0d661b1612b5e76
hajain <hajain@expediagroup.com>
2021-06-22 02:25:59 +0530

    Updates

---
+++ precache-manifest.16967e6be22c5b6962a541c2e998e8d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07453e7f9070a87470a5382adf1a7654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24319d1ed3e0bd03761f",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "d3226aeeeafa337e115d",
+    "url": "/static/css/main.45afdfcf.chunk.css"
+  },
+  {
+    "revision": "24319d1ed3e0bd03761f",
+    "url": "/static/js/2.5437fa51.chunk.js"
+  },
+  {
+    "revision": "d3226aeeeafa337e115d",
+    "url": "/static/js/main.26cd73f5.chunk.js"
+  },
+  {
+    "revision": "ecc01d1107f39ab891d4",
+    "url": "/static/js/runtime~main.8a891b78.js"
+  }
+]);
